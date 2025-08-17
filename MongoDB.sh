afb78a6f7156924c05b843f9299556d0a3c1c953
Manchem_Viswanadham <viswanadham.mancheam@gmail.com>
2025-08-18 00:37:55 +0530

    Commiting the Files

--- MongoDB.sh
+++ MongoDB.sh
@@ -1,36 +1,7 @@
 #!/bin/bash
-TIME=$(date)
-USERID=$(id -u) #Stores User UID
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
 
-LOGS_Folder="/var/log/Roboshop"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_Folder/$SCRIPT_NAME.log"
-mkdir -p $LOGS_Folder
-
-echo -e  "$R Script Executed at:$TIME $N" | tee -a $LOG_FILE # Tee command Display the content on Screen
-
-if [ $USERID -ne 0 ] #Checks Whether UID is = 0 or not
-then #!= 0 Enter into Loop
-    echo -e "$R Error:Please proceed the Installation with sudo $N" | tee -a $LOG_FILE #Prints this messages on Screen
-    exit 1 #!= 0 Don't Proceed with next command and Exit
-else #If =0 Enter into else loop
-    echo -e "$Y Please proceed the Installation $N" | tee -a $LOG_FILE #Prints this messages on Screen
-fi #Condition Ends
-
-Validate (){ #Function Definition
-    if [ $1 -eq 0 ] #Checks If Exit code equls to Zero, Yes
-    then #Enter into Loop
-        echo -e "$G $2...... is suceefull $N"  | tee -a $LOG_FILE #Prints this messages on Screen
-    else #Checks If Exit code != Zero, No
-        echo -e " $R $2 ......is failed $N" | tee -a $LOG_FILE #Prints this messages on Screen
-        exit 1 #Condition Exits and Entire Script Fails.
-    fi #Condition Ends
-}
-
+Check_Root
+app_type=mongoDB
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>>LOG_FILE
 Validate $? "Coping of MongoDB Repo"
 
@@ -48,3 +19,5 @@ Validate $? "Editing MongoDB conf file for remote connections"
 
 systemctl restart mongod
 Validate $? "MongoDB Service ReStarted" 
+
+Print_Time
