69a40faa7b9e3dd10e9fa6213734c756be094e41
Manchem_Viswanadham <viswanadham.mancheam@gmail.com>
2025-08-18 01:06:06 +0530

    Commiting Robo Shop Code

--- Common.sh
+++ Common.sh
@@ -13,6 +13,59 @@ mkdir -p $LOGS_Folder
 
 echo -e  "$R Script Executed at:$TIME $N" | tee -a $LOG_FILE # Tee command Display the content on Screen
 
+App_Setup(){
+
+    id roboshop
+    if [ $? -eq 0 ]
+    then
+        echo "Roboshop user is already created...Skipping"
+    else 
+        echo "Roboshop user is not created...Creating"
+        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+        Validate $? "Creating Roboshop user"
+    fi
+
+    mkdir  -p /app
+    Validate $? "Creating /app Dir"
+    curl -o /tmp/$app_type.zip https://roboshop-artifacts.s3.amazonaws.com/$app_type-v3.zip &>>$LOG_FILE
+    Validate $? "Downloading the Code"
+
+    rm -rf /app/*
+    cd /app &>>$LOG_FILE
+    unzip /tmp/$app_type.zip &>>$LOG_FILE
+    Validate $? "Unzip the Code"
+}
+
+NodeJS_Setup(){
+    nf module disable nodejs -y &>>$LOG_FILE
+    Validate $? "Disabeling NodeJS"
+
+    dnf module enable nodejs:20 -y &>>$LOG_FILE
+    Validate $? "Enabling NodeJS:20"
+
+    dnf install nodejs -y &>>$LOG_FILE
+    Validate $? "Installling NodeJS"
+
+    npm install &>>$LOG_FILE
+    Validate $? "Dependencies installions"
+}
+
+Systemd_Setup(){
+    cp $SCRIPT_DIR/$app_type.Service /etc/systemd/system/$app_type.service &>>$LOG_FILE
+    Validate $? "Coping $app_type Service"
+
+    systemctl daemon-reload &>>$LOG_FILE
+    Validate $? "System Daemon Reload"
+
+    systemctl enable $app_type &>>$LOG_FILE
+    Validate $? "Enabling $app_type Service"
+
+    systemctl start catalogue &>>$LOG_FILE
+    Validate $? "Starting $app_type Service"
+
+}
+
+
 Check_Root(){
     if [ $USERID -ne 0 ] #Checks Whether UID is = 0 or not
     then #!= 0 Enter into Loop
