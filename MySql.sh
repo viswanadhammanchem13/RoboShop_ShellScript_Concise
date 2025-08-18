ed252cbee3161a28aa3f073c4af6b478d42ed02a
Manchem_Viswanadham <viswanadham.mancheam@gmail.com>
2025-08-18 13:30:24 +0530

    Commiting Robo Shop Code

--- MySql.sh
+++ MySql.sh
@@ -1,35 +1,7 @@
 #!/bin/bash
-START_TIME=$(date +%s)
-USERID=$(id -u) #Stores User UID
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+app_type=mysql
 
-LOGS_Folder="/var/log/Roboshop"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_Folder/$SCRIPT_NAME.log"
-mkdir -p $LOGS_Folder
-
-echo -e  "$R Script Executed at:$START_TIME $N" | tee -a $LOG_FILE # Tee command Display the content on Screen
-
-if [ $USERID -ne 0 ] #Checks Whether UID is = 0 or not
-then #!= 0 Enter into Loop
-    echo -e "$R Error:Please proceed the Installation with sudo $N" | tee -a $LOG_FILE #Prints this messages on Screen
-    exit 1 #!= 0 Don't Proceed with next command and Exit
-else #If =0 Enter into else loop
-    echo -e "$Y Please proceed the Installation $N" | tee -a $LOG_FILE #Prints this messages on Screen
-fi #Condition Ends
-
-Validate (){ #Function Definition
-    if [ $1 -eq 0 ] #Checks If Exit code equls to Zero, Yes
-    then #Enter into Loop
-        echo -e "$G $2...... is Successful $N"  | tee -a $LOG_FILE #Prints this messages on Screen
-    else #Checks If Exit code != Zero, No
-        echo -e " $R $2 ......is failed $N" | tee -a $LOG_FILE #Prints this messages on Screen
-        exit 1 #Condition Exits and Entire Script Fails.
-    fi #Condition Ends
-}
+source ./MySql.sh
 
 echo "Please Enter Root Password to SetUp"
 read -s MYSQL_ROOT_PWD
@@ -46,7 +18,4 @@ Validate $? "Starting MYSQL Service"
 mysql_secure_installation --set-root-pass $MYSQL_ROOT_PWD &>>$LOG_FILE
 Validate $? "Creating MYSQL Password"
 
-END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME - $START_TIME ))
-
-echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+Print_Time
