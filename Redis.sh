53d60219c8468ee4e994bac9dbc5daf1bcac62b7
Manchem_Viswanadham <viswanadham.mancheam@gmail.com>
2025-08-18 12:53:44 +0530

    Commiting Robo Shop Code

--- Redis.sh
+++ Redis.sh
@@ -1,35 +1,10 @@
 #!/bin/bash
-TIME=$(date)
-USERID=$(id -u) #Stores User UID
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
 
-LOGS_Folder="/var/log/Roboshop"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_Folder/$SCRIPT_NAME.log"
-mkdir -p $LOGS_Folder
+app_type=redis
 
-echo -e  "$R Script Executed at:$TIME $N" | tee -a $LOG_FILE # Tee command Display the content on Screen
+sudo ./Common.sh
 
-if [ $USERID -ne 0 ] #Checks Whether UID is = 0 or not
-then #!= 0 Enter into Loop
-    echo -e "$R Error:Please proceed the Installation with sudo $N" | tee -a $LOG_FILE #Prints this messages on Screen
-    exit 1 #!= 0 Don't Proceed with next command and Exit
-else #If =0 Enter into else loop
-    echo -e "$Y Please proceed the Installation $N" | tee -a $LOG_FILE #Prints this messages on Screen
-fi #Condition Ends
-
-Validate (){ #Function Definition
-    if [ $1 -eq 0 ] #Checks If Exit code equls to Zero, Yes
-    then #Enter into Loop
-        echo -e "$G $2...... is suceefull $N"  | tee -a $LOG_FILE #Prints this messages on Screen
-    else #Checks If Exit code != Zero, No
-        echo -e " $R $2 ......is failed $N" | tee -a $LOG_FILE #Prints this messages on Screen
-        exit 1 #Condition Exits and Entire Script Fails.
-    fi #Condition Ends
-}
+Check_Root
 
 dnf module disable redis -y &>>$LOG_FILE
 Validate $? "Disabling Redis"
@@ -45,4 +20,6 @@ Validate $? "Edited redis.conf to accept remote connections"
 
 systemctl enable redis &>>$LOG_FILE
 systemctl start redis &>>$LOG_FILE
-Validate $? "Starting Reddis Service"
+Validate $? "Starting Redis Service"
+
+Print_Time
