306c39f3f25137369b04542332c7b7445a5fbb7d
Manchem_Viswanadham <viswanadham.mancheam@gmail.com>
2025-08-20 16:52:39 +0530

    Commiting Robo Shop Code

--- Dispatch.sh
+++ Dispatch.sh
@@ -1,74 +1,14 @@
 #!/bin/bash
-TIME=$(date)
-USERID=$(id -u) #Stores User UID
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
 
-LOGS_Folder="/var/log/Roboshop"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_Folder/$SCRIPT_NAME.log"
-SCRIPT_DIR=$PWD
+app_type=dispatch
+app_Service=Dispatch
 
-mkdir -p $LOGS_Folder
+App_Setup
 
-echo -e  "$R Script Executed at:$TIME $N" | tee -a $LOG_FILE # Tee command Display the content on Screen
+Check_Root
 
-if [ $USERID -ne 0 ] #Checks Whether UID is = 0 or not
-then #!= 0 Enter into Loop
-    echo -e "$R Error:Please proceed the Installation with sudo $N" | tee -a $LOG_FILE #Prints this messages on Screen
-    exit 1 #!= 0 Don't Proceed with next command and Exit
-else #If =0 Enter into else loop
-    echo -e "$Y Please proceed the Installation $N" | tee -a $LOG_FILE #Prints this messages on Screen
-fi #Condition Ends
+Golang_Setup
 
-Validate (){ #Function Definition
-    if [ $1 -eq 0 ] #Checks If Exit code equls to Zero, Yes
-    then #Enter into Loop
-        echo -e "$G $2...... is suceefull $N"  | tee -a $LOG_FILE #Prints this messages on Screen
-    else #Checks If Exit code != Zero, No
-        echo -e " $R $2 ......is failed $N" | tee -a $LOG_FILE #Prints this messages on Screen
-        exit 1 #Condition Exits and Entire Script Fails.
-    fi #Condition Ends
-}
+Systemd_Setup
 
-dnf install golang -y &>>$LOG_FILE
-Validate $? "Installling Python3"
-
-id roboshop
-if [ $? -eq 0 ]
-then
-    echo "Roboshop user is already created...Skipping"
-else 
-    echo "Roboshop user is not created...Creating"
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
-    Validate $? "Creating Roboshop user"
-fi
-
-mkdir  -p /app
-Validate $? "Creating /app Dir"
-
-curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch-v3.zip 
-Validate $? "Downloding the code"
-
-rm -rf /app/*
-cd /app &>>$LOG_FILE
-unzip /tmp/dispatch.zip &>>$LOG_FILE
-Validate $? "Unzip the Code"
-
-go mod init dispatch
-go get 
-go build &>>$LOG_FILE
-Validate $? "Dependencies installions"
-
-cp $SCRIPT_DIR/Dispatch.Service /etc/systemd/system/dispatch.service &>>$LOG_FILE
-Validate $? "Coping DisPatch Service"
-
-systemctl daemon-reload &>>$LOG_FILE
-Validate $? "System Daemon Reload"
-
-systemctl enable dispatch   &>>$LOG_FILE
-
-systemctl start dispatch   &>>$LOG_FILE
-Validate $? "Starting Payment Service"
+Print_Time
